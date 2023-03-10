d6ae156e8b7e32590db7f47d6017fc791d07e78d
Fabricio Richieri <fabriciorichieri@yahoo.com>
2023-03-10 10:30:36 -0300

    debugging error

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -46,6 +46,7 @@ const Login = () => {
         // ...
       })
       .catch((error) => {
+        console.log({ error });
         dispatch(setIsLoading(false));
         // Handle Errors here.
         const errorCode = error.code;
